2443287259bfdcb2ad8eeea26027b4ece1d226b5
Jack Li <xsterissmacked@gmail.com>
2014-05-11 03:01:47 -0500

    added some stuff, including config (which may change later, but is there for now)

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,10 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 # our app object
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 # loading configs
 app.config.from_object('config')
+app.config.from_pyfile('config.py')
 
 # defining db object
 db = SQLAlchemy(app)
